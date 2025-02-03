a4f88315e8f18e727765e7cd89d6f639296bca14
Leennkanakriehh <leenferas5@gmail.com>
2025-02-03 22:38:36 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,14 +1,1 @@
-<?php
-require("connect.php");
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
-$sql="INSERT INTO user (username,password) values (:username,:password)";
-$statement=$pdo->prepare($sql);
-$username=$_POST['username'];
-$password=$_POST['password'];
-$statement->bindParam(":username",$username,PDO::PARAM_STR);
-$statement->bindParam(":password",$password,PDO::PARAM_STR);
-$statement->execute();
-
-}
-?>
